25638c7b25e02052bb5e1b4c15b21e71696f625e
Diego Ferrando <diego.g.ferrando@gmail.com>
2023-04-17 16:20:38 -0300

    Initial processing of the csv file

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,3 +1,21 @@
 # frozen_string_literal: true
 
+require 'csv'
+
+def clean_zipcode(zipcode)
+  zipcode.to_s.rjust(5, '0')[0..4]
+end
+
 puts 'Event Manager initialized!'
+
+if File.exist? 'event_attendees.csv'
+  contents = CSV.open('event_attendees.csv', headers: true, header_converters: :symbol)
+  contents.each do |row|
+    name = row[:first_name]
+    zipcode = clean_zipcode(row[:zipcode])
+
+    puts "#{name} #{zipcode}"
+  end
+else
+  puts 'Missing file'
+end
