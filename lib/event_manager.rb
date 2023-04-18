67745302d3207a5e19e54eccb3282e50eab6555a
Diego Ferrando <diego.g.ferrando@gmail.com>
2023-04-18 08:12:37 -0300

    Added API connection  and info retrieval

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 require 'csv'
+require 'google/apis/civicinfo_v2'
+
+civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
 
 def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, '0')[0..4]
@@ -14,7 +18,19 @@ if File.exist? 'event_attendees.csv'
     name = row[:first_name]
     zipcode = clean_zipcode(row[:zipcode])
 
-    puts "#{name} #{zipcode}"
+    begin
+      legislators = civic_info.representative_info_by_address(
+        address: zipcode,
+        levels: 'country',
+        roles: %w[legislatorUpperBody legislatorLowerBody]
+      )
+      legislators = legislators.officials
+      legislator_names = legislators.map(&:name)
+    rescue StandardError
+      'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
+    end
+
+    puts "#{name} #{zipcode} #{legislator_names}"
   end
 else
   puts 'Missing file'
