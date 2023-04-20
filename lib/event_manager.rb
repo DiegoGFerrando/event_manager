5ca7077a5199dd2a7d7d0dd0b3de3ae3c930bd1c
Diego Ferrando <diego.g.ferrando@gmail.com>
2023-04-20 09:32:14 -0300

    Code refactoring and added personal letter

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -2,35 +2,50 @@
 
 require 'csv'
 require 'google/apis/civicinfo_v2'
+require 'erb'
 
 civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
 civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
+
+template_letter = File.read('form_letter.html')
 
 def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, '0')[0..4]
 end
 
-puts 'Event Manager initialized!'
+def legislators_by_zipcode(zip, civic_info)
+  legislators = civic_info.representative_info_by_address(
+    address: zip,
+    levels: 'country',
+    roles: %w[legislatorUpperBody legislatorLowerBody]
+  )
+  legislators = legislators.officials
+  legislator_names = legislators.map(&:name)
+  legislator_names.join(', ')
+rescue StandardError
+  'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
+end
+
+puts 'EventManager initialized.'
 
 if File.exist? 'event_attendees.csv'
-  contents = CSV.open('event_attendees.csv', headers: true, header_converters: :symbol)
+  contents = CSV.open(
+    'event_attendees.csv',
+    headers: true,
+    header_converters: :symbol
+  )
+
   contents.each do |row|
     name = row[:first_name]
+
     zipcode = clean_zipcode(row[:zipcode])
 
-    begin
-      legislators = civic_info.representative_info_by_address(
-        address: zipcode,
-        levels: 'country',
-        roles: %w[legislatorUpperBody legislatorLowerBody]
-      )
-      legislators = legislators.officials
-      legislator_names = legislators.map(&:name)
-    rescue StandardError
-      'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
-    end
+    legislators = legislators_by_zipcode(zipcode, civic_info)
 
-    puts "#{name} #{zipcode} #{legislator_names}"
+    personal_letter = template_letter.gsub('FIRST_NAME', name)
+    personal_letter.gsub!('LEGISLATORS', legislators)
+
+    puts personal_letter
   end
 else
   puts 'Missing file'
